0da3d4bbd2dfe339cd4ea503b51081abca7172eb
Oisin Martin <Oisin Martin@10.102.11.164>
2019-02-12 21:33:49 +0000

    first changes to techshop

---
+++ xampp/htdocs/TECHSHOP/index.php
@@ -0,0 +1,15 @@
+<?php include 'view/header.php'; ?>
+<link href="main.css" rel="stylesheet" type="text/css"/>
+
+<main>
+    <h1>Menu</h1>
+    <ul>
+        <li>
+            <a href="product_manager">Tech Manager</a>
+        </li>
+        <li>
+            <a href="product_catalog">Tech Catalog</a>
+        </li>
+    </ul>
+</main>
+<?php include 'view/footer.php'; ?>
---
+++ xampp/htdocs/TECHSHOP/view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>My Tech Shop</title>
+    <link href="../main.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>My Tech Shop</h1></header>
+       
